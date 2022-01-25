53dac27c280ff653bc851b93a5c8387bc973ac1d
echo <echo@arbitraryexecution.com>
2022-01-25 10:48:18 -0500

    AE-960: Moving functions from common.js into utils.js module.

--- monitor-function-calls/src/utils.js
+++ monitor-function-calls/src/utils.js
@@ -138,12 +138,33 @@ function checkLogAgainstExpression(expressionObject, log) {
   return comparisonFunction(argValue, operator, operand);
 }
 
+function getAbi(abiName) {
+  // eslint-disable-next-line global-require,import/no-dynamic-require
+  const { abi } = require(`../abi/${abiName}`);
+  return abi;
+}
+
+// helper function that identifies key strings in the args array obtained from transaction parsing
+// these key-value pairs will be added to the metadata as function args
+// all values are converted to strings so that BigNumbers are readable
+function extractFunctionArgs(args) {
+  const functionArgs = {};
+  Object.keys(args).forEach((key) => {
+    if (Number.isNaN(Number(key))) {
+      functionArgs[key] = args[key].toString();
+    }
+  });
+  return functionArgs;
+}
+
 module.exports = {
-    isNumeric,
-    isAddress,
-    addressComparison,
-    booleanComparison,
-    bigNumberComparison,
-    parseExpression,
-    checkLogAgainstExpression,
+  getAbi,
+  extractFunctionArgs,
+  isNumeric,
+  isAddress,
+  addressComparison,
+  booleanComparison,
+  bigNumberComparison,
+  parseExpression,
+  checkLogAgainstExpression,
 };
