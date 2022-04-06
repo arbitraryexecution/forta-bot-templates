dab1aba40f2e109991619ca9585460b32f48c2e2
Colin Davidson <colin@arbitraryexecution.com>
2022-04-06 16:51:45 -0700

    Begin tinkering with modularization ideas

---
+++ godzilla.js
@@ -0,0 +1,42 @@
+const {
+  Finding, FindingSeverity, FindingType, ethers, getEthersProvider,
+} = require('forta-agent');
+
+const agents = [];
+function generateAllAgents(config) {
+	// TODO(cloin): Fill out agents from config
+}
+generateAllAgents(config);
+
+function provideInitialize(data) {
+	return async function initialize() {
+		for agent in agents {
+			// TODO(cloin): Fill out agents with Forta data
+		}
+	}
+}
+
+function handleAllTransactions(data) {
+	return async function handleTransaction() {
+		for agent in agents {
+			// TODO(cloin): Process all agents with transactions
+		}
+	}
+}
+
+function handleAllBlocks(data) {
+	return async function handleBlock() {
+		for agent in agents {
+			// TODO(cloin): Process all agents with blocks
+		}
+	}
+}
+
+module.exports = {
+  provideInitialize,
+  initialize: provideInitialize(initializeData),
+  provideHandleTransaction,
+  handleTransaction: provideHandleTransaction(initializeData),
+  provideHandleBlock,
+  handleBlock: provideHandleBlock(initializeData),
+};
